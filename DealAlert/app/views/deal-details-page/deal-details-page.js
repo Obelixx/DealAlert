aa8c5feb2deaecb8c12f6432f6cff09865ad992d
Obelixx <Obelixx@users.noreply.github.com>
2016-02-19 22:33:00 +0200

    added bower.json

--- DealAlert/app/views/deal-details-page/deal-details-page.js
+++ DealAlert/app/views/deal-details-page/deal-details-page.js
@@ -1,4 +1,7 @@
-var everlive = require("~/../node_modules/everlive-edk/src/Everlive");
+var everlive = require("~/bower_components/everlive/min/everlive.all.min.js");
+var vmModule = require("../../view-models/main-page-view-model");
 function pageLoaded(args) {
+	var page = args.object;
+    page.bindingContext = vmModule.mainViewModel;
 }
 exports.pageLoaded = pageLoaded;
