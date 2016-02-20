514323df732f44f0b2a5e9dc08acc4d964ac1f84
Obelixx <Obelixx@users.noreply.github.com>
2016-02-20 19:00:20 +0200

    more deal details

--- DealAlert/app/dataProviders/everlive.js
+++ DealAlert/app/dataProviders/everlive.js
@@ -2,17 +2,20 @@ var Everlive = require('../bower_components/everlive/min/everlive.all.min');
 var navigationModule = require('../utils/navigation');
 var dataProvider;
 
-dataProvider = new Everlive({
-	appId: 'xw7rpl6g52f4b0sj',
-	scheme: 'https',
-	authentication: {
-		persist: true,
-		onAuthenticationRequired: function() {
-			alert('Your access token has expired. Please log in.');
-			// Redirect to log-in page
-			navigationModule.goToMainPage();
+if (!global.dataProvider) {
+	dataProvider = new Everlive({
+		appId: 'xw7rpl6g52f4b0sj',
+		scheme: 'https',
+		authentication: {
+			persist: true,
+			onAuthenticationRequired: function() {
+				alert('Your access token has expired. Please log in.');
+				// Redirect to log-in page
+				navigationModule.goToMainPage();
+			}
 		}
-	}
-});
+	});
+	global.dataProvider = dataProvider;
+}
 
-module.exports = dataProvider;
+module.exports = global.dataProvider;
