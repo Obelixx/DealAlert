9abf35a0cae72ca866c2ce3a7c089a05d543cc1a
matioa <matioa@gmail.com>
2016-02-21 18:56:06 +0200

    Fixed loading images in detail view.

--- DealAlert/app/view-models/deal-details-page-view-model.js
+++ DealAlert/app/view-models/deal-details-page-view-model.js
@@ -17,34 +17,34 @@ class DealDetailsModel extends observable.Observable {
         // this.loadDataAction();
     }
 
-    loadDataAction() {
-        let that = this;
-        let deals = dataProvider.data('Deals');
-        that.set("isLoading", true);
+    // loadDataAction() {
+    //     let that = this;
+    //     let deals = dataProvider.data('Deals');
+    //     that.set("isLoading", true);
 
-        deals.get(null, function(data) {
-            let pictureSrc = "https://bs1.cdn.telerik.com/v1/xw7rpl6g52f4b0sj/" + data.result[0].Picture;
-            console.log(pictureSrc);
-            that.set("PictureSrc", pictureSrc);
-            that.set("Title", data.result[0].Title);
-            that.set("Description", data.result[0].Description);
-            that.set("ValidUntil", moment(data.result[0].ValidUntil).format('MMM Do YY'));
-            that.set("PromoPrice", data.result[0].PromoPrice);
-            that.set("RegularPrice", data.result[0].RegularPrice);
-            that.set("isLoading", false);
+    //     deals.get(null, function(data) {
+    //         let pictureSrc = "https://api.everlive.com/v1/xw7rpl6g52f4b0sj/files/" + data.result[0].Picture +'/download';
+    //         console.log(pictureSrc);
+    //         that.set("PictureSrc", pictureSrc);
+    //         that.set("Title", data.result[0].Title);
+    //         that.set("Description", data.result[0].Description);
+    //         that.set("ValidUntil", moment(data.result[0].ValidUntil).format('MMM Do YY'));
+    //         that.set("PromoPrice", data.result[0].PromoPrice);
+    //         that.set("RegularPrice", data.result[0].RegularPrice);
+    //         that.set("isLoading", false);
 
-        }, function(err) {
-            console.log("error");
-            // alert(err.message);
-            that.set("isLoading", false);
-        });
-    }
+    //     }, function(err) {
+    //         console.log("error");
+    //         // alert(err.message);
+    //         that.set("isLoading", false);
+    //     });
+    // }
 
     loadItem(item) {
         let that = this;
         that.set("isLoading", true);
 
-        let pictureSrc = "https://bs1.cdn.telerik.com/v1/xw7rpl6g52f4b0sj/" + item.Picture;
+        let pictureSrc = "https://api.everlive.com/v1/xw7rpl6g52f4b0sj/files/" + item.Picture +'/download';
         console.log(pictureSrc);
         that.set("PictureSrc", pictureSrc);
         that.set("Title", item.Title);
